a773de8ef744a050df072ed0fa1d81e155e9b051
Bruce Szostak <36425377+Bruce-Szo@users.noreply.github.com>
2021-12-04 11:58:27 -0700

    Sound Stuff
    Added Hellsound library for sound and music.

--- Not A Bug Game Jam 2021/Assets/Scripts/AudioManager.cs
+++ Not A Bug Game Jam 2021/Assets/Scripts/AudioManager.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Hellmade.Sound;
 
 public class AudioManager : MonoBehaviour
 {
-    public AudioSource currentMusic;
     public AudioClip[] soundtrack;
+    public AudioClip shootFX;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentMusic = this.GetComponent<AudioSource>();
         PlayRandomMusic();
     }
 
     void PlayRandomMusic() {
-        currentMusic.clip = soundtrack[Random.Range(0, soundtrack.Length)];
-        currentMusic.Play();
+        int backgroundMusicID = EazySoundManager.PlayMusic(soundtrack[Random.Range(0, soundtrack.Length)], 0.1f, true, false, 1, 1);
+    }
+
+    void PlayShootEffect() {
+        int shootID = EazySoundManager.PlaySound(shootFX, 0.2f);
     }
 }
