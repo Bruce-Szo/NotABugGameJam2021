b1a8172183196356fccf7dc445f59253fd8dddff
KnightLane11 <74807418+KnightLane11@users.noreply.github.com>
2021-12-05 19:49:20 -0700

    Final

--- Not A Bug Game Jam 2021/Assets/Scripts/BackGroundManager.cs
+++ Not A Bug Game Jam 2021/Assets/Scripts/BackGroundManager.cs
@@ -9,6 +9,6 @@ public class BackGroundManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<SpriteRenderer>().sprite = backgrounds[Random.Range(0, 32)];
+        gameObject.GetComponent<SpriteRenderer>().sprite = backgrounds[Random.Range(0, 24)];
     }
 }
